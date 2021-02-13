c4d9aef83291da638ca776988247ab7240f2cce7
Mkrefting <maxkrefting@yahoo.co.uk>
2021-02-13 18:45:23 +0000

    Initial Commit

---
+++ DemoCameraApp/DemoCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCameraAppApp.swift
+//  DemoCameraApp
+//
+//  Created by Max Krefting on 13/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
